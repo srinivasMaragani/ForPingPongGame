76a2c9bda339764fcdd783c40e3c1cd545b8a9b7
Srinivas Maragani <srinivasaraomaragani1729@gmail.com>
2023-06-12 18:42:54 +0530

    logic

---
+++ ForPingPongGame/ForPingPongGame/AddPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForPingPongGame
+{
+	public class AddPlayer
+	{
+		string[] A = new string[4];
+		string[] B = new string[4];
+		string[] C = new string[4];
+		string[] D = new string[4];
+		string[] E = new string[4];
+		string[] F = new string[4];
+		string[] G = new string[4];
+		string[] H = new string[4];
+
+		public void adding(string x)
+		{
+			
+		   
+		}
+
+
+		
+
+	}
+}
---
+++ ForPingPongGame/ForPingPongGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		List<string>[] totalTeams = new List<string>[8];
+
+		for (int i = 0; i < totalTeams.Length; i++)
+		{
+			totalTeams[i] = new List<string>();
+		}
+
+		Console.WriteLine("Enter 32 names:");
+
+		var filledTeam = new List<int>();
+		Random random = new Random();
+
+		for (int i = 0; i < 32; i++)
+		{
+			int randomIndex = random.Next(0, 8);
+			if (!filledTeam.Contains(randomIndex))
+			{
+				if (totalTeams[randomIndex].Count < 4)
+				{
+					Console.Write($"Enter name {i + 1}: ");
+					string name = Console.ReadLine();
+					totalTeams[randomIndex].Add(name);
+				}
+				else
+				{
+					filledTeam.Add(randomIndex);
+					i--; 
+				}
+			}
+			else
+			{
+				i--;
+			}
+		}
+
+		Console.WriteLine("Teams:");
+
+		for (int i = 0; i < totalTeams.Length; i++)
+		{
+			Console.WriteLine($"Teams {i + 1}: {string.Join(", ", totalTeams[i])}");
+		}
+	}
+}
